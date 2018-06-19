b14cd499caf519824255be4bb57f37de4ffe022f
Vikas Gupta <vikas.gupta@accoliteindia.com>
2018-06-20 05:15:02 +0530

    updating static data in right KRA now...

--- lib/Feedback.dart
+++ lib/Feedback.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'kra.dart';
 
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
 class FeedBack extends StatefulWidget {
   final Kra kra;
 
@@ -23,10 +26,44 @@ class FeedBack extends StatefulWidget {
     appBar: new AppBar(
       title: new Text("Goal - " + kra.goal),
     ),
-    body: new Padding(
-      padding: new EdgeInsets.all(16.0),
-      child: new Text("Feedback - " + kra.remarks )
-    )
-  );
+    body: new Column(
+          children: [
+            new Text("Feedback => "+ kra.remarks,
+            style: TextStyle(
+              fontSize: 28.0,
+              color: Colors.deepPurple,
+              
+            ),),
+            new IconButton(
+              icon: new Icon(
+                Icons.arrow_back,
+                color: Colors.green,
+                size: 48.0,
+              ),
+              onPressed: () {Navigator.pop(context); },
+            ),
+                        new IconButton(
+              icon: new Icon(
+                Icons.bug_report ,
+                color: Colors.green,
+                size: 48.0,
+              ),
+              onPressed: () async {
+              print('Update data now....!!!!');
+              String url = "https://script.google.com/macros/s/AKfycbw62MiIrGBtrfx59fizysETG-H2sPWUu8Z-VRI1SVT2_d91oZk/exec?id=19vljbQ5P-uU1d1yXrO6l5BP7CiwTzmu0UIKWR8v2_Po&sheet=Quarter-4&row="; 
+              url = url + (kra.id + 13).toString() + "&rating=&remarks=" + kra.remarks+"updt1"; 
+              print(url);
+              var response = await http.get(
+                // Uri.encodeFull("https://script.google.com/a/accoliteindia.com/macros/s/AKfycbwd6Esnt_dqsxTOExgNiQD0-YUn7UCVVZ_XyzZaqMI/dev?id=19vljbQ5P-uU1d1yXrO6l5BP7CiwTzmu0UIKWR8v2_Po&sheet=Quarter-4&row=20&rating=&remarks=updatedbyvikas"),
+                Uri.encodeFull(url),
+                headers: {
+                "Accept": "application/json"
+              }
+              );
+              },
+            ),
+          ], 
+      ),
+   );
 }   
 }
--- lib/main.dart
+++ lib/main.dart
@@ -28,21 +28,23 @@ class HomePageState extends State<HomePage> {
     // //  write data -- 
     //     // Uri.encodeFull("https://script.google.com/macros/s/AKfycbzOUIMN82DmBLgWsmp-AKXchuUHYMJYjKZ_d7iZirFdmNJOaeQ/exec?name=SaiTatsat Gupta&country=India&id=1hXOg8TONsTa5mhgQ9tjk9Sj75xGVWvWsuJKt_qf3PMY"),
     // print("reaching out to the server!!!");
-    //   var response = await http.get(
-    //      Uri.encodeFull("https://script.google.com/macros/s/AKfycbw62MiIrGBtrfx59fizysETG-H2sPWUu8Z-VRI1SVT2_d91oZk/exec?id=19vljbQ5P-uU1d1yXrO6l5BP7CiwTzmu0UIKWR8v2_Po&sheet=Quarter-4"),
-    //   headers: {
-    //     "Accept": "application/json"
-    //   }
-    // );
-    // print ("Response is = " + response.body.toString() );
+      var response = await http.get(
+         Uri.encodeFull("https://script.google.com/macros/s/AKfycbw62MiIrGBtrfx59fizysETG-H2sPWUu8Z-VRI1SVT2_d91oZk/exec?id=19vljbQ5P-uU1d1yXrO6l5BP7CiwTzmu0UIKWR8v2_Po&sheet=Quarter-4"),
+        //  Uri.encodeFull("https://script.google.com/a/accoliteindia.com/macros/s/AKfycbwd6Esnt_dqsxTOExgNiQD0-YUn7UCVVZ_XyzZaqMI/dev?id=19vljbQ5P-uU1d1yXrO6l5BP7CiwTzmu0UIKWR8v2_Po&sheet=Quarter-4"),
+        //  Uri.encodeFull("https://script.google.com/macros/s/AKfycbw62MiIrGBtrfx59fizysETG-H2sPWUu8Z-VRI1SVT2_d91oZk/exec?id=19vljbQ5P-uU1d1yXrO6l5BP7CiwTzmu0UIKWR8v2_Po&sheet=Quarter-4&row=21&rating=&remarks=update-from-postman"),
+      headers: {
+        "Accept": "application/json"
+      }
+    );
+    print ("Response is = " + response.body.toString() );
 
-    tmpJSON = '[{"Goal":"Business Growth ","Brief_Objectives_":"","Target":"","Weightage":0.2,"Goals_achieved":"","Remarks":""},{"Goal":"Revenue growth (Quarter to Quarter)","Brief_Objectives_":"","Target":0.1,"Weightage":0.1,"Goals_achieved":"","Remarks":"Rev Growth Feedback"},{"Goal":"Customer Success","Brief_Objectives_":"","Target":0.9,"Weightage":0.1,"Goals_achieved":"","Remarks":"new feedback"},{"Goal":"Org Impact","Brief_Objectives_":"","Target":"","Weightage":0.2,"Goals_achieved":"","Remarks":""},{"Goal":"Number of Interviews conducted/ Campus Visits ","Brief_Objectives_":"","Target":30,"Weightage":0.05,"Goals_achieved":"","Remarks":""},{"Goal":"PMCoE initiative, Ilearn,Branding , Unplugged , Fun Friday , Process impovements,Location wide Activities  & others ","Brief_Objectives_":"","Target":"Very active participation","Weightage":0.15,"Goals_achieved":"","Remarks":""},{"Goal":"Delivery","Brief_Objectives_":"","Target":"","Weightage":0.45,"Goals_achieved":"","Remarks":""},{"Goal":"Utilization","Brief_Objectives_":"","Target":0.92,"Weightage":0.05,"Goals_achieved":"","Remarks":""},{"Goal":"Coding,Problem Solving, Quality, Design,","Brief_Objectives_":"","Target":"High Quality","Weightage":0.25,"Goals_achieved":"","Remarks":""},{"Goal":"Architeture Score Card ","Brief_Objectives_":"","Target":90,"Weightage":0.15,"Goals_achieved":"","Remarks":""},{"Goal":"Retention","Brief_Objectives_":"","Target":"","Weightage":0.15,"Goals_achieved":"","Remarks":""},{"Goal":"Retention","Brief_Objectives_":"","Target":0.9,"Weightage":0.15,"Goals_achieved":"","Remarks":""}]';
+    // tmpJSON = '[{"Goal":"Business Growth ","Brief_Objectives_":"","Target":"","Weightage":0.2,"Goals_achieved":"","Remarks":""},{"Goal":"Revenue growth (Quarter to Quarter)","Brief_Objectives_":"","Target":0.1,"Weightage":0.1,"Goals_achieved":"","Remarks":"Rev Growth Feedback"},{"Goal":"Customer Success","Brief_Objectives_":"","Target":0.9,"Weightage":0.1,"Goals_achieved":"","Remarks":"new feedback"},{"Goal":"Org Impact","Brief_Objectives_":"","Target":"","Weightage":0.2,"Goals_achieved":"","Remarks":""},{"Goal":"Number of Interviews conducted/ Campus Visits ","Brief_Objectives_":"","Target":30,"Weightage":0.05,"Goals_achieved":"","Remarks":""},{"Goal":"PMCoE initiative, Ilearn,Branding , Unplugged , Fun Friday , Process impovements,Location wide Activities  & others ","Brief_Objectives_":"","Target":"Very active participation","Weightage":0.15,"Goals_achieved":"","Remarks":""},{"Goal":"Delivery","Brief_Objectives_":"","Target":"","Weightage":0.45,"Goals_achieved":"","Remarks":""},{"Goal":"Utilization","Brief_Objectives_":"","Target":0.92,"Weightage":0.05,"Goals_achieved":"","Remarks":""},{"Goal":"Coding,Problem Solving, Quality, Design,","Brief_Objectives_":"","Target":"High Quality","Weightage":0.25,"Goals_achieved":"","Remarks":""},{"Goal":"Architeture Score Card ","Brief_Objectives_":"","Target":90,"Weightage":0.15,"Goals_achieved":"","Remarks":""},{"Goal":"Retention","Brief_Objectives_":"","Target":"","Weightage":0.15,"Goals_achieved":"","Remarks":""},{"Goal":"Retention","Brief_Objectives_":"","Target":0.9,"Weightage":0.15,"Goals_achieved":"","Remarks":""}]';
 
 
     this.setState(() {
       // print('decoding json now...');
-      // data = json().decode(response.body);
-      data = json().decode(tmpJSON);
+      data = json().decode(response.body);
+      // data = json().decode(tmpJSON);
       // print(data.toString());
     });
     // print(data[1]["Goal"]);
